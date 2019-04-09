f81813bc4e35074013765f15d18dc844c07f3022
rubenfondonalcalde <rubenfondon@gmail.com>
2019-04-09 17:42:12 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+//Import stylesheets
+import './style.css'
 
+//Gráfico de Barras. Mi 1º grafico
+var visualization = d3plus.viz()
+.container("#viz1")
+.data([{"year": 1991, "name":"alpha", "value": 15},
+{"year": 1992, "name":"alpha", "value": 20},])
+.type("bar")
+.id("name")
+.x("year")
+.y("value")
+.draw()
 
